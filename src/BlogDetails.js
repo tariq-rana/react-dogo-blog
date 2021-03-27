680c304919c4d8161ebab1c27a53ad6089ed7dd1
tariq-rana <tariq_rana@yahoo.com>
2021-03-27 23:05:15 +0700

    t :q

---
+++ src/BlogDetails.js
@@ -0,0 +1,36 @@
+import { useParams, useHistory } from "react-router-dom";
+import useFetch from './useFetch';
+
+
+const BlogDetails = () => {
+    const { id } = useParams();
+    const {data:blog,error, isPending} = useFetch(`http://localhost:8000/blogs/${id}`);
+    const history = useHistory();
+
+    const deleteBlog = (e) =>{
+
+        fetch('http://localhost:8000/blogs/' + id, {
+            method: 'DELETE'
+        }).then( () =>{
+            console.log('Blog deleted');
+            history.push('/');
+        })
+    }
+
+    return (
+        <div className="blog-details">
+           { error && <div>{error}</div>}
+           { isPending && <div>Loading ....</div>}
+           { blog && (
+                <article>
+                    <h2>{ blog.title }</h2>  
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                    <button onClick={ deleteBlog }>Delete</button>
+                </article>
+           )}
+        </div>
+    )
+}
+
+export default BlogDetails;
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import useFetch from "./useFetch";
+import BlogList from "./BlogList";
+
+
+const Home = () =>{
+    
+    const {data:blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+
+    return(
+        <div className="home">
+            { error && <div>{error}</div>}
+            { isPending && <div>Loading ....</div>}
+            { blogs &&  <BlogList title="All Blogs!" blogs={ blogs } />}
+        </div>
+    );
+}
+
+export default Home;
